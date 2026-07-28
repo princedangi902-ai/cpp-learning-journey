1706b7963948c335f8b84047be940f98d007450f
princedangi902-ai <princedangi902@gmail.com>
2026-07-28 17:08:47 +0530

    diamond pattern added

---
+++ patternprinting/diamond.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout << "enter a number: ";
+    cin >> a;
+
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= a-i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            cout << "*";
+        }
+        for (int l = 2; l <= i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 1; i <= a; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " ";
+        }
+        for (int k = 1; k <= a-i; k++)
+        {
+            cout << "*";
+        }  
+         for (int l = 2; l <= a-i; l++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
